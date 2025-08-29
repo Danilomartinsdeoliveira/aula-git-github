f25513c685c4c869a8d9e1189b1ecc74c5772e17
Danilomartinsdeoliveira <danilooliveira.161103@gmail.com>
2025-08-28 21:50:22 -0300

    alteraçao pedida na atividade 5

--- teste/teste.java
+++ teste/teste.java
@@ -3,6 +3,9 @@ package teste;
 public class Teste {
     public static void main(String[] args) {
         int num1 = 3, num2 = 5, soma;
+        
         soma = num1 + num2;
-        System.out.println("A soma é: " + soma);
-    }
+        
+        System.out.println(soma);
+    }
+}
